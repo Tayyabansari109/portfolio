0b5b86c73f8ce05445014915cd0fb8a09717090e
Tayyabansari109 <ta759777@gmail.com>
2026-05-07 18:28:45 +0500

    update

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
 <footer className="py-2">
-<div className="container">
+{/* <div className="container"> */}
     <div className="row">
         <div className="col text-white text-center">&copy; {new Date().getFullYear()} All Rights Reserved</div>
     </div>
-</div>
+{/* </div> */}
 </footer>
   )
 }
